753db9f1e20792e69a46a19123d2c6f7037519e8
michel19 <michel@soajs.org>
2016-12-28 17:11:14 +0200

    adding swagger sample after generation

---
+++ swaggerSample/config.js
@@ -0,0 +1,107 @@
+'use strict';
+var services = {
+	"type": "service",
+	"dbs": {
+	},
+	"prerequisites": {
+		"cpu": '',
+		"memory": ''
+	},
+	"serviceName": "swaggerSample",
+	"serviceGroup": "sample",
+	"serviceVersion": 1,
+	"servicePort": 4061,
+	"requestTimeout": 30,
+	"requestTimeoutRenewal": 5,
+	"extKeyRequired": false,
+	"session": false,
+	"oauth": false,
+	"logger": false,
+	"inputmask": true,
+	"cookieParser": false,
+	"methodOverride": false,
+	"awareness": false,
+	"bodyParser": false,
+	"errors": {
+		400: "Error connecting to the database",
+		401: "invalid id",
+		402: "missing required field"
+	},
+	"schema": {
+		// "commonFields": { this shows that we can have common fields, but in this example we don't need it.
+		// 	// "id": {
+		// 	// 	"source": ['query.id'],
+		// 	// 	"required": true,
+		// 	// 	"validation": {"type": "string"}
+		// 	// },
+		// },
+		"get": {
+			"/": {
+				"_apiInfo": {
+					"l": "get item form a specified cart",
+					"group": "users",
+					"groupMain": true
+				},
+				"mw": __dirname + "/lib/mw/get.js",
+				"userId": {
+					"source": ['query.userId'],
+					"required": true,
+					"validation": {
+						"type": "integer"
+					}
+				}
+			}
+		},
+		"post": {
+			"/": {
+				"_apiInfo": {
+					"l": "set a cart",
+					"group": "users"
+				},
+				"mw": __dirname + "/lib/mw/post.js",
+				"userId": {
+					"source": ['body.userId'],
+					"required": true,
+					"validation": {
+						"type": "integer"
+					}
+				},
+				"items": {
+					"source": ['body.items'],
+					"required": true,
+					"validation": {
+						"type": "array",
+						"items": {
+							"type": "object",
+							"properties": {
+								"productId":{
+									"type": "string",
+									"required": true,
+									"minLength": 8,
+									"maxLength": 8
+								},
+								"price": {
+									"type": "number",
+									"required": true,
+									"minimum": 0
+								},
+								"currency": {
+									"type": "string",
+									"required": true
+								},
+								"quantity": {
+									"type": "integer",
+									"required": true,
+									"minimum": 1
+								}
+							}
+						},
+						"minItems": 1,
+						"uniqueItems": true
+					}
+				}
+			}
+		}
+	}
+};
+module.exports = services;
