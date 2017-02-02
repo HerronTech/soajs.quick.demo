8a46f382faba59457feffe22dff7ff2c6a41e903
michel19 <michel@soajs.org>
2017-02-02 18:17:46 +0200

    changed the generated folder schema

--- config.js
+++ config.js
@@ -1,98 +1,27 @@
-'use strict';
-var config = {
-	"type": "service",
-	"prerequisites": {
-		"cpu": '',
-		"memory": ''
-	},
-	"swagger": true,
-	"dbs": [
-		{
-			prefix: "",
-			name: "swaggerSampleDB",
-			multitenant: false,
-			model: "mongo"
-		}],
-	"serviceName": "swaggerSample",
-	"serviceGroup": "sample",
-	"serviceVersion": 1,
-	"servicePort": 4061,
-	"requestTimeout": 30,
-	"requestTimeoutRenewal": 5,
-	"extKeyRequired": false,
-	"errors": {
-		400: "Error connecting to the database",
-		401: "invalid id",
-		402: "missing required field"
-	},
-	"schema": {
-		"commonFields": { //this shows that we can have common fields, but in this example we don't need it.
-			"id": {
-				"source": ['query.id'],
-				"required": true,
-				"validation": {"type": "string"}
-			}
-		},
-		"get": {
-			"/": {
-				"_apiInfo": {
-					"l": "get users",
-					"group": "users",
-					"groupMain": true
-				},
-				"mw": __dirname + "/lib/mw/get.js"
-			}
-		},
-		"post": {
-			"/": {
-				"_apiInfo": {
-					"l": "set a cart",
-					"group": "users"
-				},
-				"mw": __dirname + "/lib/mw/post.js",
-				"imfv": {
-					"custom": {
-						"items": {
-							"source": ['body.items'],
-							"required": true,
-							"validation": {
-								"type": "array",
-								"items": {
-									"type": "object",
-									"properties": {
-										"userId":{
-											"type": "integer",
-											"required": true,
-											"format": "int32"
-										},
-										"productId": {
-											"type": "string",
-											"required": true
-										},
-										"price": {
-											"type": "number",
-											"required": true,
-											"minimum": 0
-										},
-										"currency": {
-											"type": "string",
-											"required": true
-										},
-										"quantity": {
-											"type": "integer",
-											"required": true,
-											"minimum": 1
-										}
-									}
-								},
-								"minItems": 1,
-								"uniqueItems": true
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+"use strict";
+module.exports = {
+  "type": "service",
+  "prerequisites": {
+    "cpu": " ",
+    "memory": " "
+  },
+  "swagger": true,
+  "dbs": [
+    {
+      "name": "myDb",
+      "model": "mongo",
+      "multitenant": false
+    }
+  ],
+  "serviceName": "myService",
+  "serviceGroup": "myGroup",
+  "serviceVersion": 1,
+  "servicePort": 4265,
+  "requestTimeout": 30,
+  "requestTimeoutRenewal": 5,
+  "extKeyRequired": true,
+  "oauth": false,
+  "session": true,
+  "errors": {},
+  "schema": {}
 };
-module.exports = config;
