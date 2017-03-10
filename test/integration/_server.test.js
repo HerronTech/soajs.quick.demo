35504606f76091644494f5e17282db48610df962
mikehajj <mike.hajj@gmail.com>
2017-03-10 11:24:17 +0200

    removed test folder.

--- test/integration/_server.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe("Calling Server Integration", function () {
-	it("Running Tests", function (done) {
-		require("./demoservice.test.js");
-		done();
-	});
-});
--- test/unit/_server.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe("Calling Server Unit", function () {
-	it("Running Tests", function (done) {
-		require("./demoservice.test.js");
-		done();
-	});
-});
